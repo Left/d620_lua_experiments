c50c87e193edccee388d84ad03739cded30f44af
vridosh <vridosh@fanzcall.com>
2017-10-10 16:14:20 +0300

    first experiment

--- hello.lua
+++ hello.lua
@@ -12,6 +12,10 @@
 -- print('body',body)
 
 local p = io.popen('ls /dev/usb')  --Open directory look for files, save data in p. By giving '-type f' as parameter, it returns all files.
-for file in p:lines() do                         --Loop through all files
-    print(file)
+for fileName in p:lines() do                         --Loop through all files
+    print(fileName)
+    file = io.open("/dev/usb/" + fileName, "rb")
+    local buf = f:read(8)
+    print(buf)
+    f:close()
 end
