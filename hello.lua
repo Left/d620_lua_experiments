fd16767ea2d3fedf815488faf1df3f077d66b6e8
vridosh <vridosh@fanzcall.com>
2017-10-10 16:06:07 +0300

    first experiment

--- hello.lua
+++ hello.lua
@@ -6,7 +6,12 @@
 -- To change this template use File | Settings | File Templates.
 --
 
-local http=require'socket.http'
-body,c,l,h = http.request('http://w3.impa.br/~diego/software/luasocket/http.html')
-print('status line',l)
-print('body',body)
+-- local http=require'socket.http'
+-- body,c,l,h = http.request('http://w3.impa.br/~diego/software/luasocket/http.html')
+-- print('status line',l)
+-- print('body',body)
+
+local p = io.popen('ls /dev/usb')  --Open directory look for files, save data in p. By giving '-type f' as parameter, it returns all files.
+for file in p:lines() do                         --Loop through all files
+    print(file)
+end
