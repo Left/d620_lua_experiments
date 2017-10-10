cdb2ea08c0d7fe2e6ef4157e9ce40bc6f046cfd3
vridosh <vridosh@fanzcall.com>
2017-10-10 14:55:44 +0300

    first experiment

--- hello.lua
+++ hello.lua
@@ -6,4 +6,7 @@
 -- To change this template use File | Settings | File Templates.
 --
 
-print("Hello")
+local http=require'socket.http'
+body,c,l,h = http.request('http://w3.impa.br/~diego/software/luasocket/http.html')
+print('status line',l)
+print('body',body)
